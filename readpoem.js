fd33afa7fda9b6dd885ff6bfb5a73cf0e715e1c1
nisiviloria <nisiashley22@gmail.com>
2024-08-27 14:43:18 +0800

    Clean code Added

--- readpoem.js
+++ readpoem.js
@@ -1,8 +1,8 @@
-console.log('\n\tWelcome to Poem Reader!');
-console.log('\n   The Winds of Change - Ariana Banos\n');
-
 const fs = require('fs');
 const readline = require('readline');
+
+console.log('\n\tWelcome to Poem Reader!');
+console.log('\n   The Winds of Change - Ariana Banos\n');
 
 const userInput = readline.createInterface({
   input: process.stdin,
@@ -10,43 +10,44 @@ const userInput = readline.createInterface({
 });
 
 function askForLineNumber() {
-  userInput.question('Enter a line number (or "q" to quit): ', (input) => {
-    if (input.toLowerCase() === 'q') {
-      console.log('Goodbye! Have a good day!');
-      userInput.close();
-      return;
+  userInput.question('Enter a line number (or "q" to quit): ', handleUserInput);
+}
+
+function handleUserInput(input) {
+  if (input.toLowerCase() === 'q') {
+    console.log('Goodbye! Have a good day!');
+    return userInput.close();
+  }
+
+  const lineNumber = Number(input);
+  if (isNaN(lineNumber) || lineNumber <= 0) {
+    console.log('Please enter a valid line number.');
+    return askForLineNumber();
+  }
+
+  readPoemFile(lineNumber);
+}
+
+function readPoemFile(lineNumber) {
+  fs.readFile('poem.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err.message);
+      return askForLineNumber();
     }
 
-    const lineNumber = Number(input);
-
-    if (isNaN(lineNumber) || lineNumber <= 0) {
-      console.log('Please enter a valid line number.');
-      askForLineNumber();
-      return;
+    const lines = data.split('\n');
+    if (lineNumber > lines.length) {
+      console.log('Line number exceeds the number of lines in the file.');
+    } else if (lines[lineNumber - 1].trim() === '') {
+      console.log('Error: The selected line is blank or contains only a new line.');
+    } else {
+      console.log(lines[lineNumber - 1]);
     }
 
-
-    fs.readFile('poem.txt', 'utf8', (err, data) => {
-      if (err) {
-        console.log('Error reading file:', err);
-        askForLineNumber();
-        return;
-      }
-
-      const lines = data.split('\n');
-      if (lineNumber > lines.length) {
-        console.log('Line number exceeds the number of lines in the file.');
-      } else {
-        const line = lines[lineNumber - 1];
-        if (line !== undefined) {
-          console.log(line);
-        } else {
-          console.log('Line number does not exist.');
-        }
-      }
-      askForLineNumber(); 
-    });
+    askForLineNumber();
   });
 }
 
 askForLineNumber();
+
+module.exports = { askForLineNumber, handleUserInput, readPoemFile };
